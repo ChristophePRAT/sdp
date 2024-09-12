62bc350080489ad5200d000593f22cdd64808ae3
Christophe Prat <christopheprat@icloud.com>
2024-09-12 23:38:47 +0200

    v1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,89 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import 'katex/dist/katex.min.css';
+import Latex from 'react-latex-next';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [p, setP] = useState("");
+  const [root1, setR1] = useState(0);
+  const [root2, setR2] = useState(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const generateP = () => {
+    const r1 = Math.floor(Math.random() * 20) - 10;
+    const r2 = Math.floor(Math.random() * 20) - 10;
+    const a = Math.floor(Math.random() * 20) - 10;
+
+    // P = a(x - r1)(x - r2)
+    const b = -a * (r1 + r2);
+    const c = a * r1 * r2;
+    setR1(r1);
+    setR2(r2);
+    if (a === 0) {
+      generateP();
+      return;
+    } else if (b ===0) {
+      if (c === 0) {
+        setP(`$P = ${a !== 1 ? a : ''}x^2 $`);
+        return;
+      }
+      setP(`$P = ${a !== 1 ? a : ''}x^2 ${c > 0 ? '+' : ''} ${c !== 1 ? c : ''} $`);
+      return;
+    } else if (c === 0) {
+      setP(`$P = ${a !== 1 ? a : ''}x^2 ${b > 0 ? '+' : ''} ${b !== 1 ? b : ''}x $`);
+      return;
+    } 
+    setP(`$P = ${a !== 1 ? a : ''}x^2 ${b > 0 ? '+' : ''}${b !== 1 ? b : ''}x ${c > 0 ? '+' : ''}${c !== 1 ? c : ''} $`);
+
+  }
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    
+    const predR1 = parseFloat(e.target[0].value);
+    const predR2 = parseFloat(e.target[1].value);
+    if ((predR1 === root1 && predR2 === root2) || (predR1 === root2 && predR2 === root1)) {
+      alert("Bravo! Vous avez trouvé les racines de P");
+      generateP();
+    } else {
+      alert("Désolé, vous avez fait une erreur");
+    }
+    e.target[0].value = "";
+    e.target[1].value = "";
+  }
+
+  useEffect(() => {
+    generateP();
+  }, []);
+  return (
+    <div className="grid items-center justify-items-center min-h-screen place-content-center min-w-screen">
+      <main className="flex flex-col align-middle items-center text-center justify-center m-auto min-w-screen">
+        <h1 className="text-4xl my-3 font-medium tracking-wider">Trouver les racines de:</h1>
+        <h2 className="text-5xl border-2 p-4 my-4">
+          <Latex>{p}</Latex>
+        </h2>
+        <form className="p-6 bg-black shadow-md rounded-lg" onSubmit={(e) => onSubmit(e)}>
+          <h3 className="text-xl mb-4">Racines réelles:</h3>
+          <div className="flex justify-between gap-20 mb-8 font-bold w-1/2">
+            <input
+              type="text"
+              maxLength={3}
+              className="border-b-2 border-gray-300 text-5xl p-3 bg-black text-white focus:outline-none focus:ring-0 w-24 text-center"
+              placeholder="0"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+            <input
+              type="text"
+              maxLength={3}
+              className="border-b-2 border-gray-300 text-5xl p-3 bg-black text-white focus:outline-none focus:ring-0 w-24 text-center"
+              placeholder="0"
+            />
+          </div>
+          <input
+            type="submit"
+            className="bg-white  text-2xl text-black font-bold p-3 px-5 rounded-lg cursor-pointer hover:bg-gray-200 transition duration-300"
+            value="Valider"
+          />
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
